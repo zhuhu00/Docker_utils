46645b5fd0fc13112857b3a30a5b6269fb2f0736
Huge_alien <zhuhu00@foxmail.com>
2022-03-04 15:45:58 +0800

    update

--- docker-cuda100-ros-ubuntu16/ros_entrypoint.sh
+++ docker-cuda100-ros-ubuntu16/ros_entrypoint.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
+ 
 set -e
 
-# setup ros environment
-echo "ROS workspace: /root/catkin_ws"
-source "/opt/ros/kinetic/setup.bash"
+# 设置ros环境
+source "/opt/ros/melodic/setup.bash"
 source "/root/catkin_ws/devel/setup.bash"
 
-echo "compile caffe-segnet"
-# cd /root/caffe-segnet
-# [ ! -d "build" ] && mkdir build 
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE=Release  -DCMAKE_INSTALL_PREFIX=/usr/local
-# make -j${nproc} && make install
 
+# 可以对一些库进行构建和install
 
-echo "Build DSSLAM"
-# cd /root/catkin_ws/src/DS-SLAM
-# bash DS_SLAM_BUILD.sh
+echo "================Docker Env Ready================"
+
+cd /root/catkin_ws
 
 exec "$@"
--- docker-cuda110-ros-ubuntu20/ros_entrypoint.sh
+++ docker-cuda110-ros-ubuntu20/ros_entrypoint.sh
@@ -6,13 +6,6 @@ set -e
 source "/opt/ros/noetic/setup.bash"
 source "/root/catkin_ws/devel/setup.bash"
 
-cd /root/catkin_ws/src/faster-lio
-rm -rf build
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j$(nproc)
-
 
 # 可以对一些库进行构建和install
 
